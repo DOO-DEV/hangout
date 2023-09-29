144f7d5a7ce957d47463bf737b7fa7a6b2a91d65
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-29 10:56:52 +0330

    refactor: change the columns of message table

--- repository/postgres/migrations/1695383997_add_messages_table.sql
+++ repository/postgres/migrations/1695383997_add_messages_table.sql
@@ -1,7 +1,8 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS "messages" (
     "id" SERIAL PRIMARY KEY,
-    "chat_id" TEXT REFERENCES "chats"("id"),
+    "chat_id" UUID REFERENCES "chats"("id"),
+    "sender_id" TEXT REFERENCES "users"("id"),
     "content" TEXT,
     "type" TEXT,
     "timestamp" TIMESTAMP DEFAULT NOW()
